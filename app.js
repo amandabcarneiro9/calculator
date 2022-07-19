1df1e5a4a21dfb92c1ddd98c438745b7deb64ff8
Amanda <amandabcarneiro9@gmail.com>
2022-07-19 11:50:27 +0200

    getting number os display when clicked

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+// const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 keys.addEventListener("click", (e) => {
   const key = e.target;
   const action = key.dataset.action;
+  const keyContent = key.textContent;
+  const displayedNum = display.textContent;
+
   if (e.target.matches("button")) {
   }
   if (!action) {
-    console.log("number key!");
+    if (displayedNum === "0") {
+      display.textContent = keyContent;
+    } else {
+      display.textContent = displayedNum + keyContent;
+    }
   }
   if (
     action === "add" ||
@@ -16,5 +25,16 @@ keys.addEventListener("click", (e) => {
     action === "divide"
   ) {
     console.log("operator key!");
+  }
+  if (action === "decimal") {
+    console.log("decimal key!");
+  }
+
+  if (action === "clear") {
+    console.log("clear key!");
+  }
+
+  if (action === "calculate") {
+    console.log("equal key!");
   }
 });
